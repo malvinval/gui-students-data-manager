34f0513a6262fd7cfb859b6eb32be03c4a1967a7
malvinval <malvinval09@gmail.com>
2022-08-05 14:55:47 +0700

    Initial commit

---
+++ DatabaseMahasiswa/src/main/java/com/vin/databasemahasiswa/DatabaseMahasiswa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.vin.databasemahasiswa;
+
+/**
+ *
+ * @author vin
+ */
+public class DatabaseMahasiswa {
+
+    public static void main(String[] args) {
+        System.out.println("Please compile the DBMahasiswa.java file !");
+    }
+}
